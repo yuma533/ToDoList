4c1fb36cdc340654b333b8bc56fdf30a35b30800
yuma533 <o.g.w.y.u.m2262@gmail.com>
2023-01-31 12:52:18 +0900

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by UMA on 2023/01/31.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
